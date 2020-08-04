d16139c1945458ad286a14ffa5df22bb95252b55
Nathan DeSpain <nathandespain@Nathans-MacBook-Pro.local>
2020-08-04 12:48:31 -0700

    Boilerplate abstract animal class and subclasses built, now to do something with them

---
+++ animalKingdom/src/animalKingdom/Animal.java
@@ -0,0 +1,49 @@
+package animalKingdom;
+
+abstract class Animal{
+
+  //variables that Animal subclasses
+  static int maxId = 0;
+  protected int id;
+  protected String name;
+  protected int yearDiscovered;
+  protected int food = 10;
+
+  public Animal(String name, int yearDiscovered){
+    maxId++;
+
+    this.id = maxId;
+    this.name = name;
+    this.yearDiscovered = yearDiscovered;
+  }
+
+  //methods that Animal subclasses will need. these will be available to all animals
+  public void eat(int foodEaten){
+    food = food + foodEaten;
+  }
+  abstract String move();//"abstract" allows us to add behavior specific to the subclasses
+  abstract String breath();
+  abstract String reproduce();
+
+  //getters let's us control how variables are obtained
+  public int getId(){
+    return id;
+  }
+  public String getName(){
+    return name;
+  }
+  public int getYearDiscovered(){
+    return yearDiscovered;
+  }
+  public int getFood(){
+    return food;
+  }
+
+  //Setters let's us control how variables are changed
+  public void setName(String name){
+    this.name = name;
+  }
+  public void setYearDiscovered(int yearDiscovered){
+    this.yearDiscovered = yearDiscovered;
+  }
+}
---
+++ animalKingdom/src/animalKingdom/Main.java
@@ -0,0 +1,11 @@
+package animalKingdom;
+
+public class Main{
+
+  public static void workWithData(){
+
+  }
+  public static void main(String[] args){
+    workWithData();
+  }
+}
